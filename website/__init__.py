d061c64ab90434a8a9f3bebfaa48a6fbb2df97c8
MDragonCCI <108028168+MDragonCCI@users.noreply.github.com>
2023-07-05 11:43:02 +0100

    Campaign extractor v0.1
    Lates version of campaign extractor v0.1

--- website/__init__.py
+++ website/__init__.py
@@ -6,13 +6,13 @@ def create_app():
     app.config["SECRET_KEY"] = "AKJkfjdkalsfsadfasdf"
     
 
-    from .revenue import revenue, revenue_report
+    from .revenue import revenue
     from .views import home
 
     
     app.register_blueprint(revenue, url_prefix='/')
     app.register_blueprint(home, url_prefix='/')
-    app.register_blueprint(revenue_report, url_prefix='/')
+    
 
 
     return app
--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 
 home = Blueprint('home', __name__)
 
 
-@home.route("/")
+@home.route("/", methods=["GET", "POST"])
 def home_objects():
+	if request.method == "POST":
+		return redirect(url_for("revenue.revenue_func"))
 	return render_template("home.html")
