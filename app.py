1ebe6b3b0d0fff6340cae06a337b5ef9137f9fe6
MDragonCCI <108028168+MDragonCCI@users.noreply.github.com>
2023-08-15 10:54:09 +0100

    v1.12
    Adding security package:
    Secure Headers

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from website import create_app
-
+import secure
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run(debug = True)
+
+secure_headers = secure.Secure()
+ 
+@app.after_request
+def set_secure_headers(response):
+    secure_headers.framework.flask(response)
+    return response
+
 
